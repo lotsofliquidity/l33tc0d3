931ca3330bbcaf891b1b4777dcd588dfb7a06d26
lotsofliquidity <haydenstrading@gmail.com>
2025-12-15 21:32:03 +1000

    tried number of subarrays and read about fixed window

---
+++ arrays-and-strings/sliding-window/examples/find-best-subarray.py
@@ -0,0 +1,4 @@
+# Example 4: Given an integer array nums and an integer k, find the sum of the subarray with the largest sum whose length is k.
+
+def find_best_subarray_v1(nums, k):
+    return 
--- arrays-and-strings/sliding-window/examples/subarray-product-less-than-k.py
+++ arrays-and-strings/sliding-window/examples/subarray-product-less-than-k.py
@@ -5,3 +5,24 @@
 #    For example, given the input nums = [10, 5, 2, 6], k = 100, the answer is 8. The subarrays with products less than k are:
 #
 #    [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+
+def numSubarrayProductLessThanK_v1(nums, k):
+    if k <= 1: # if value 1 then would loop forever because curr >= k would never be false
+        return 0
+
+    ans = left = 0
+    curr = 1 # because you can't times by zero
+
+    for right in range(len(nums)):
+        curr *= nums[right]
+
+        while  curr >= k:
+            curr //= nums[left]
+            left += 1
+
+        ans += right - left + 1
+
+    return ans
+
+result = numSubarrayProductLessThanK_v1([10, 5, 2, 6], 100)
+print(result)
