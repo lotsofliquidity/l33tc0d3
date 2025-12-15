6f93f751044df65c2c65b9a6cf4db8b53faf99fe
lotsofliquidity <haydenstrading@gmail.com>
2025-12-15 21:33:01 +1000

    updating comments

--- arrays-and-strings/sliding-window/examples/subarray-product-less-than-k.py
+++ arrays-and-strings/sliding-window/examples/subarray-product-less-than-k.py
@@ -1,10 +1,9 @@
 #    Example 3: 713. Subarray Product Less Than K.
-#
 #    Given an array of positive integers nums and an integer k, return the number of subarrays where the product of all the elements in the subarray is strictly less than k.
-#
 #    For example, given the input nums = [10, 5, 2, 6], k = 100, the answer is 8. The subarrays with products less than k are:
-#
 #    [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+
+# --- This is a different kind of sliding window from what we've done before. ---
 
 def numSubarrayProductLessThanK_v1(nums, k):
     if k <= 1: # if value 1 then would loop forever because curr >= k would never be false
